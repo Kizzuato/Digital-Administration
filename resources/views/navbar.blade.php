99967f8cac4c479d2840c98b02083cfc5bb17678
Kizzuato <kyyajuara@gmail.com>
2025-03-11 23:32:16 +0700

    nambahin halaman login, dashboard, sidebar, sama navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,2 @@
+<nav class="bg-orange-600 dark:bg-orange-500 text-white p-4 flex justify-between items-center">
+</nav>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,9 @@
+<aside class="w-64 bg-orange-600 dark:bg-orange-500 text-white min-h-screen p-6">
+    <h2 class="text-lg font-bold mb-6">Menu</h2>
+    <ul class="space-y-4">
+        <li><a href="#" class="block py-2 px-4 rounded-lg hover:bg-orange-700">Dashboard</a></li>
+        <li><a href="#" class="block py-2 px-4 rounded-lg hover:bg-orange-700">Data Rekapitulasi</a></li>
+        <li><a href="#" class="block py-2 px-4 rounded-lg hover:bg-orange-700">Pengguna</a></li>
+        <li><a href="#" class="block py-2 px-4 rounded-lg hover:bg-orange-700">Pengaturan</a></li>
+    </ul>
+</aside>
